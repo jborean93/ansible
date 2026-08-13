de93e9912c24a2f7d6bb0f86719e1901762e9ce0
Martin Krizek <martin.krizek@gmail.com>
2026-08-13 12:59:29 +0200

    Allow for numeric secrets

--- lib/ansible/module_utils/secrets.py
+++ lib/ansible/module_utils/secrets.py
@@ -1,7 +1,10 @@
 from ansible.module_utils._internal._secrets import _secret_masker
 
-def register_secret(value: str) -> str:
-    return _secret_masker.register_secret_text(value)
+def register_secret(value: str | int | float) -> str | int | float:
+    orig_value = value
+    _secret_masker.register_secret_text(str(value))
+    return orig_value
+
 
 def mask_secrets(value: str, *, mask_placeholder: str = '<secret>') -> str:
     return _secret_masker.mask_string(value, mask_placeholder=mask_placeholder)
