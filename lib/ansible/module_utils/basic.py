751b1bb34353b848f81bd687efa4399838e636e4
Martin Krizek <martin.krizek@gmail.com>
2026-08-17 14:21:48 +0200

    Deprecate the current API and remove usage

--- lib/ansible/module_utils/basic.py
+++ lib/ansible/module_utils/basic.py
@@ -139,6 +139,8 @@ from ansible.module_utils.common.sys_info import (
 )
 from ansible.module_utils.common.parameters import (
     env_fallback,
+    remove_values,
+    sanitize_keys,
     DEFAULT_TYPE_VALIDATORS,
     PASS_VARS,
     PASS_BOOLS,
@@ -230,6 +232,76 @@ def get_all_subclasses(cls):
         help_text="Use `get_all_subclasses()` from `ansible.module_utils.common._utils` instead.",
     )
     return list(_get_all_subclasses(cls))
+
+
+def heuristic_log_sanitize(data, no_log_values=None):
+    """ Remove strings that look like passwords from log messages """
+    deprecate(
+        msg="The `heuristic_log_sanitize()` function from `ansible.module_utils.basic` is deprecated.",
+        version="2.25",
+        help_text="Secret values are now masked automatically. Use functions from `ansible.module_utils.secrets` if you need to handle secrets manually.",
+    )
+    # Currently filters:
+    # user:pass@foo/whatever and http://username:pass@wherever/foo
+    # This code has false positives and consumes parts of logs that are
+    # not passwds
+
+    # begin: start of a passwd containing string
+    # end: end of a passwd containing string
+    # sep: char between user and passwd
+    # prev_begin: where in the overall string to start a search for
+    #   a passwd
+    # sep_search_end: where in the string to end a search for the sep
+    data = to_native(data)
+
+    output = []
+    begin = len(data)
+    prev_begin = begin
+    sep = 1
+    while sep:
+        # Find the potential end of a passwd
+        try:
+            end = data.rindex('@', 0, begin)
+        except ValueError:
+            # No passwd in the rest of the data
+            output.insert(0, data[0:begin])
+            break
+
+        # Search for the beginning of a passwd
+        sep = None
+        sep_search_end = end
+        while not sep:
+            # URL-style username+password
+            try:
+                begin = data.rindex('://', 0, sep_search_end)
+            except ValueError:
+                # No url style in the data, check for ssh style in the
+                # rest of the string
+                begin = 0
+            # Search for separator
+            try:
+                sep = data.index(':', begin + 3, end)
+            except ValueError:
+                # No separator; choices:
+                if begin == 0:
+                    # Searched the whole string so there's no password
+                    # here.  Return the remaining data
+                    output.insert(0, data[0:prev_begin])
+                    break
+                # Search for a different beginning of the password field.
+                sep_search_end = begin
+                continue
+        if sep:
+            # Password was found; remove it.
+            output.insert(0, data[end:prev_begin])
+            output.insert(0, '********')
+            output.insert(0, data[begin:sep + 1])
+            prev_begin = begin
+
+    output = ''.join(output)
+    if no_log_values:
+        output = remove_values(output, no_log_values)
+    return output
 
 
 def _load_params():
